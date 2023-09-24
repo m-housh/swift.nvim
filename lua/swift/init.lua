9f9b838662c27da36a3a6a3e531d14e65259b141
Michael Housh <m-housh@users.noreply.github.com>
2023-09-24 14:43:10 -0400

    Added readme

--- lua/swift/init.lua
+++ lua/swift/init.lua
@@ -5,27 +5,34 @@ local augroupname = "SwiftNvim"
 local autocmd = vim.api.nvim_create_autocmd
 local usercmd = vim.api.nvim_create_user_command
 
+local function set_formatting()
+  -- Creates an auto command group to register our
+  -- auto commands in.
+  local group = vim.api.nvim_create_augroup(augroupname, { clear = true })
+
+  -- Format swift files when written to a buffer.
+  autocmd(
+    "BufWritePost",
+    {
+      group = group,
+      pattern = "*.swift",
+      command = ":silent exec '!swift-format --in-place %'"
+    }
+  )
+end
+
 -- Setup the plugin.
 swift.setup = function(opts)
   opts = opts or {}
 
   local setup_formatting = opts["setup_formatting"]
 
-  if setup_formatting or (setup_formatting ~= nil) then
-    -- Creates an auto command group to register our
-    -- auto commands in.
-    local group = vim.api.nvim_create_augroup(augroupname, { clear = true })
-
-    -- Format swift files when written to a buffer.
-    autocmd(
-      "BufWritePost",
-      {
-        group = group,
-        pattern = "*.swift",
-        command = ":silent exec '!swift-format --in-place %'"
-      }
-    )
+  if (setup_formatting ~= nil) then
+    setup_formatting()
+  elseif (setup_formatting == true) then
+    setup_formatting()
   end
+
 
   -- Exposes a command that can be used to build a
   -- swift package manager project.
